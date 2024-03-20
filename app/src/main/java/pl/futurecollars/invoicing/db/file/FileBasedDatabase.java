a59baa28e43f3efb6238b954cc93d5f272a2f95b
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-03-20 19:31:01 +0100

    implemented methods in new classes without tests for them

--- app/src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
@@ -1,33 +1,88 @@
 package pl.futurecollars.invoicing.db.file;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
+import pl.futurecollars.invoicing.utils.FilesService;
+import pl.futurecollars.invoicing.utils.JsonService;
 
-public class FileBasedDatabase implements Database {
+public class FileBasedDatabase<T> implements Database {
+
+  private final Path databasePath;
+  private final IdService idService;
+  private final FilesService filesService;
+  private final JsonService jsonService;
+  private final Class<T> clazz;
+
+  public FileBasedDatabase(Path databasePath, IdService idService, FilesService filesService, JsonService jsonService, Class<T> clazz) {
+    this.databasePath = databasePath;
+    this.idService = idService;
+    this.filesService = filesService;
+    this.jsonService = jsonService;
+    this.clazz = clazz;
+  }
+
   @Override
   public int save(Invoice invoice) {
-    return 0;
+    invoice.setId(idService.getNextIdAndIncrement());
+    filesService.appendLineToFile(databasePath, jsonService.toJson(invoice));
+    return invoice.getId();
   }
 
   @Override
   public Optional<Invoice> getById(int id) {
-    return Optional.empty();
+    return filesService.readAllLines(databasePath)
+        .stream()
+        .filter(line -> containsId(line, id))
+        .map(line -> (Invoice) jsonService.toObject(line, clazz))
+        .findFirst();
   }
 
   @Override
   public List<Invoice> getAll() {
-    return null;
+    return filesService.readAllLines(databasePath)
+        .stream()
+        .map(line -> (Invoice) jsonService.toObject(line, clazz))
+        .collect(Collectors.toList());
   }
 
   @Override
   public void update(int id, Invoice updatedInvoice) {
+    List<String> allLines = filesService.readAllLines(databasePath);
+    List<String> listUpdatedInvoice = allLines
+        .stream()
+        .filter(line -> !containsId(line, id))
+        .collect(Collectors.toList());
 
+    Optional<Invoice> invoice = getById(id);
+    if (invoice.isPresent()) {
+      Invoice newInvoice = invoice.get();
+      newInvoice.setId(id);
+      newInvoice.setDate(updatedInvoice.getDate());
+      newInvoice.setBuyer(updatedInvoice.getBuyer());
+      newInvoice.setSeller(updatedInvoice.getSeller());
+      newInvoice.setEntries(updatedInvoice.getEntries());
+      listUpdatedInvoice.add(jsonService.toJson(newInvoice));
+    } else {
+      throw new IllegalArgumentException("Id " + id + " does not exist");
+    }
+    filesService.writeLinesToFile(databasePath, listUpdatedInvoice);
   }
 
   @Override
   public void delete(int id) {
+    List<String> allLines = filesService.readAllLines(databasePath);
+    List<String> listWithoutInvoiceFromId = allLines
+        .stream()
+        .filter(line -> !containsId(line, id))
+        .collect(Collectors.toList());
+    filesService.writeLinesToFile(databasePath, listWithoutInvoiceFromId);
+  }
 
+  private boolean containsId(String line, int id) {
+    return line.contains("{\"id\":" + id + ",");
   }
 }
--- app/src/main/java/pl/futurecollars/invoicing/utils/FilesService.java
+++ app/src/main/java/pl/futurecollars/invoicing/utils/FilesService.java
@@ -1,4 +1,42 @@
 package pl.futurecollars.invoicing.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class FilesService {
+
+  public void appendLineToFile(Path path, String line) {
+    try {
+      Files.write(path, (line + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to append line to file", e);
+    }
+  }
+
+  public void writeToFile(Path path, String line) {
+    try {
+      Files.write(path, line.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to write to file", e);
+    }
+  }
+
+  public void writeLinesToFile(Path path, List<String> lines) {
+    try {
+      Files.write(path, lines, StandardOpenOption.TRUNCATE_EXISTING);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to all lines to file", e);
+    }
+  }
+
+  public List<String> readAllLines(Path path) {
+    try {
+      return Files.readAllLines(path);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed read all lines from file", e);
+    }
+  }
 }
