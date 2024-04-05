6748af370ef4073f1730e4d5fc3a5dacfd764ba7
Irenkia <72923191+Irenkia@users.noreply.github.com>
2024-04-05 23:32:54 +0200

    Task6 - Test your REST services (#6)
    * added dependencies for spock-spring and changed the project structure
    
    * added folder github with main.yml file
    
    * created a test for the InvoiceApplication class and a Stepwise test for the InvoiceController class
    
    * increased test coverage to 85% and added the InvoiceControllerTest class with separate tests for various non-standard cases

--- src/main/java/pl/futurecollars/invoicing/InvoiceApplication.java
+++ src/main/java/pl/futurecollars/invoicing/InvoiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class InvoiceApplication {
 
   public static void main(String[] args) {
 
-    SpringApplication.run(App.class, args);
+    SpringApplication.run(InvoiceApplication.class, args);
   }
 }
