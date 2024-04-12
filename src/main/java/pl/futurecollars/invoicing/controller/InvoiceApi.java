38727343db8584a7ba1395d7ea09ae951b3c9136
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-04-12 11:00:17 +0200

    extracted the interface InvoiceApi from InvoiceController and move all annotations there

---
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceApi.java
@@ -0,0 +1,46 @@
+package pl.futurecollars.invoicing.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import pl.futurecollars.invoicing.model.Invoice;
+
+@RequestMapping("/invoices")
+@Api(tags = {"invoice-controller"})
+public interface InvoiceApi {
+  @ApiOperation(value = "Get list of all invoices")
+  @GetMapping(produces = {"application/json;charset=UTF-8"})
+  @ResponseStatus(HttpStatus.CREATED)
+  ResponseEntity<List<Invoice>> getAll();
+
+  @ApiOperation(value = "Add new invoice to system")
+  @PostMapping
+  @ResponseStatus(HttpStatus.OK)
+  int addInvoice(@RequestBody Invoice invoice);
+
+  @ApiOperation(value = "Get invoice by id")
+  @GetMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
+  @ResponseStatus(HttpStatus.OK)
+  ResponseEntity<Optional<Invoice>> findById(@PathVariable("id") int id);
+
+  @ApiOperation(value = "Update invoice with given id")
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  ResponseEntity<Optional<Invoice>> updateInvoice(@PathVariable("id") int id, @RequestBody Invoice invoice);
+
+  @ApiOperation(value = "Delete invoice with given id")
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  ResponseEntity<Optional<Invoice>> deleteInvoice(@PathVariable("id") int id);
+}
--- src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,72 +1,46 @@
 package pl.futurecollars.invoicing.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
-@RequestMapping("/invoices")
-@Api(tags = {"invoice-controller"})
-public class InvoiceController {
+@AllArgsConstructor
+public class InvoiceController implements InvoiceApi {
 
-  @Autowired
   private final InvoiceService service;
 
-  @Autowired
-  public InvoiceController(InvoiceService service) {
-    this.service = service;
-  }
-
-  @ApiOperation(value = "Get list of all invoices")
-  @GetMapping(produces = {"application/json;charset=UTF-8"})
-  @ResponseStatus(HttpStatus.CREATED)
+  @Override
   public ResponseEntity<List<Invoice>> getAll() {
     return ResponseEntity.ok().body(service.getAll());
   }
 
-  @ApiOperation(value = "Add new invoice to system")
-  @PostMapping
-  @ResponseStatus(HttpStatus.OK)
+  @Override
   public int addInvoice(@RequestBody Invoice invoice) {
     return service.save(invoice);
   }
 
-  @ApiOperation(value = "Get invoice by id")
-  @GetMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
-  @ResponseStatus(HttpStatus.OK)
+  @Override
   public ResponseEntity<Optional<Invoice>> findById(@PathVariable("id") int id) {
     return Optional.ofNullable(service.getById(id))
         .map(invoice -> ResponseEntity.ok().body(invoice))
         .orElse(ResponseEntity.notFound().build());
   }
 
-  @ApiOperation(value = "Update invoice with given id")
-  @PutMapping("/{id}")
-  @ResponseStatus(HttpStatus.OK)
+  @Override
   public ResponseEntity<Optional<Invoice>> updateInvoice(@PathVariable("id") int id, @RequestBody Invoice invoice) {
     return Optional.of(service.update(id, invoice))
         .map(ResponseEntity.ok()::body)
         .orElse(ResponseEntity.notFound().build());
   }
 
-  @ApiOperation(value = "Delete invoice with given id")
-  @DeleteMapping("/{id}")
-  @ResponseStatus(HttpStatus.OK)
+  @Override
   public ResponseEntity<Optional<Invoice>> deleteInvoice(@PathVariable("id") int id) {
     return Optional.of(service.delete(id))
         .map(ResponseEntity.ok()::body)
