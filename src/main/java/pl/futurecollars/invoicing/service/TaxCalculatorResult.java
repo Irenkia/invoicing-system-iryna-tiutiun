6d004ff03e3be9129d489d99c461a13f85c8f37b
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-04-17 10:44:48 +0200

    created model TaxCalculatorResult

---
+++ src/main/java/pl/futurecollars/invoicing/service/TaxCalculatorResult.java
@@ -0,0 +1,22 @@
+package pl.futurecollars.invoicing.service;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaxCalculatorResult {
+
+  private BigDecimal income;
+  private BigDecimal costs;
+  private BigDecimal earnings;
+
+  private BigDecimal incomingVat;
+  private BigDecimal outgoingVat;
+  private BigDecimal vatToPay;
+}
