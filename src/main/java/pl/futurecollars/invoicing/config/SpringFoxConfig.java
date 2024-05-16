b375e0f81096084a31bafe8379c1d4657e5555bb
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-05-16 17:19:54 +0200

    added new 2 tags in Docket for SWAGGER UI and changed profile files

--- src/main/java/pl/futurecollars/invoicing/config/SpringFoxConfig.java
+++ src/main/java/pl/futurecollars/invoicing/config/SpringFoxConfig.java
@@ -22,7 +22,9 @@ public class SpringFoxConfig {
         .paths(PathSelectors.any())
         .build()
         .tags(
-            new Tag("invoice-controller", "Controller used to list / add / update / delete invoices.")
+            new Tag("invoice-controller", "Controller used to list / add / update / delete invoices."),
+            new Tag("company-controller", "Controller used to list / add / update / delete companies."),
+            new Tag("tax-controller", "Controller used to calculate taxes.")
         )
         .apiInfo(apiInfo());
   }
