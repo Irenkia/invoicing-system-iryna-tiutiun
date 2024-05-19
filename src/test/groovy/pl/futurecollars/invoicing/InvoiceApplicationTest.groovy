7748fd5c88e040d71b9cdd8f20b04394030b5e64
Irenkia <72923191+Irenkia@users.noreply.github.com>
2024-05-19 19:29:47 +0200

    Task16 - Implement companies UI (#16)
    * created a list of companies fetched from REST Web Service API
    
    * created form to add new companies with table refresh automatically
    
    * added Bootstrap v5.0
    
    * changed InvoiceApplicationTest class

--- src/test/groovy/pl/futurecollars/invoicing/InvoiceApplicationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/InvoiceApplicationTest.groovy
@@ -2,6 +2,7 @@ package pl.futurecollars.invoicing
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import pl.futurecollars.invoicing.service.company.CompanyService
 import pl.futurecollars.invoicing.service.invoice.InvoiceService
 import pl.futurecollars.invoicing.service.tax.TaxCalculatorService
 import spock.lang.Specification
@@ -13,11 +14,19 @@ class InvoiceApplicationTest extends Specification {
     private InvoiceService invoiceService;
 
     @Autowired
+    private CompanyService companyService;
+
+    @Autowired
     TaxCalculatorService taxCalculatorService
 
     def "invoice service id created"() {
         expect:
         invoiceService
+    }
+
+    def "company service id created"() {
+        expect:
+        companyService
     }
 
     def "tax calculator service id created"() {
