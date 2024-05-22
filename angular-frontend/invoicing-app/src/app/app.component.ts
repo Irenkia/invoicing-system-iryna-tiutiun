edd7f34c35826ce9f07027c118efaa378fa6a21b
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-05-22 13:24:11 +0200

    created a list of companies read from the mock collection

--- angular-frontend/invoicing-app/src/app/app.component.ts
+++ angular-frontend/invoicing-app/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Company } from './company';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'invoicing-app';
+
+  companies: Company[] = [
+    {
+      taxIdentificationNumber: '111-111-11-11',
+      address: 'ul. Bukowinska 24d/7 02-703 Warszawa, Polska',
+      name: 'First company Ltd.',
+      pensionInsurance: 1111.11,
+      healthInsurance: 111.11,
+    },
+    {
+      taxIdentificationNumber: '222-222-22-22',
+      address: 'ul. Bukowinska 24d/7 02-703 Warszawa, Polska',
+      name: 'Second company Ltd.',
+      pensionInsurance: 2222.22,
+      healthInsurance: 222.22,
+    },
+  ];
 }
---
+++ angular-frontend/invoicing-app/src/app/company.ts
@@ -0,0 +1,8 @@
+export interface Company{
+    taxIdentificationNumber: string;
+    address: string;
+    name: string;
+    pensionInsurance: number;
+    healthInsurance: number;
+}
+
